5403b8ac29a9f386b76e4caaa84eabd71cd05220
eriklinda <51276656+eriklinda@users.noreply.github.com>
2021-06-19 13:20:46 +0200

    Vanquish-site

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#1e1e1e';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
